3d31274787b5db1f2c19ecae7f2b57b1dd334795
Gabe Joseph <gjoseph92@gmail.com>
2016-11-14 16:43:37 -0700

    Don't track dist/

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup, find_packages
 
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, "README.md"), encoding= "utf-8") as f:
+    long_description = f.read()
+
 setup(
     name= "soundDB",
     version= "0.0.1",
     description= "A library for the NPS Natural Sounds & Night Skies Division to make going from data-somewhere-on-disk to data-ready-for-processing as painless as possible",
+    # long_description= long_description,
     url= "https://github.com/nationalparkservice/soundDB",
     author= "Gabe Joseph",
     author_email= "gabriel_joseph@partner.nps.gov",
